9c94aa1e909d2b9375afac23caaea67a3148da28
Eunice Fiorella <euniceancajima@gmail.com>
2021-03-20 17:04:09 -0500

    Modularizando funciones

---
+++ src/components/means.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+const objValidate = [];
+
+module.exports.isDir = ((pathDir) => {
+  const status = fs.statSync(pathDir);
+  return status.isDirectory();
+});
+module.exports.isFile = ((pathFile) => {
+  const status = fs.statSync(pathFile);
+  return status.isFile();
+});
+
+const awaitStatusValidate = ((esto, ht, pathValidate) => {
+  return new Promise((resolve) => {
+    ht.get(esto, (res) => {
+      if (res.statusCode === 404) {
+        resolve({
+          'href': esto, 'text': res.statusCode, 'file': pathValidate, 'status': 'Fail',
+        });
+      } else {
+        resolve({
+          'href': esto, 'text': res.statusCode, 'file': pathValidate, 'status': 'Ok',
+        });
+      }
+    });
+  });
+});
+
+const awaitStatus = ((esto, ht, pathLink) => {
+  return new Promise((resolve) => {
+    ht.get(esto, (res) => {
+      resolve({ 'href': esto, 'text': res.statusCode, 'file': pathLink });
+    });
+  });
+});
+
+module.exports.returnObject = ((filterHttp, pathLink) => {
+  for (const elem of filterHttp) {
+    if (elem.startsWith('https')) {
+      const prueba1 = awaitStatus(elem, https, pathLink);
+      objValidate.push(prueba1);
+    } else {
+      const prueba1 = awaitStatus(elem, http, pathLink);
+      objValidate.push(prueba1);
+    }
+  }
+  return objValidate;
+});
+module.exports.validateLinks = ((filterHttp, pathValidate) => {
+  for (const elem of filterHttp) {
+    if (elem.startsWith('https')) {
+      const prueba1 = awaitStatusValidate(elem, https, pathValidate);
+      objValidate.push(prueba1);
+    } else {
+      const prueba1 = awaitStatusValidate(elem, http, pathValidate);
+      objValidate.push(prueba1);
+    }
+  }
+  return objValidate;
+});

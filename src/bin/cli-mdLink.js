052f0f2a65af6af02c68800df11e8e54c7e86a81
Eunice Fiorella <euniceancajima@gmail.com>
2021-03-30 04:19:42 -0500

    Lógica CLI, consumo de api mdLinks

---
+++ src/bin/cli-mdLink.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+const path = require('path');
+const { mdLinks } = require('../mdLinks.js');
+const { statsMd, statsValidateMd, links } = require('../components/means-cli.js');
+
+if (process.argv.length > 5) {
+  console.log('Exceso de comandos, ¡Por favor ingrese comandos correctos!');
+} else if (process.argv.length < 3) {
+  console.log('Ingrese un Path directorio o archivo .md');
+} else {
+  const li = path.normalize(process.argv[2]);
+  const resolvePath = path.resolve(li);
+  if (process.argv.length === 3) {
+    mdLinks(resolvePath).then((msg) => {
+      links(msg);
+    });
+  } else if (process.argv.length === 4) {
+    if (process.argv[3].toLowerCase() === '--validate') {
+      mdLinks(resolvePath, true).then((msg) => {
+        links(msg);
+      });
+    } else if (process.argv[3].toLowerCase() === '--stats') {
+      const stats = statsMd();
+      console.log(stats);
+    } else {
+      console.log('Ingrese comando válido, --stats o --validate');
+    }
+  } else if ((process.argv[3].toLowerCase() === '--validate' && process.argv[4].toLowerCase() === '--stats') || (process.argv[4].toLowerCase() === '--validate' && process.argv[3].toLowerCase() === '--stats')) {
+    const statsValidate = statsValidateMd();
+    console.log(statsValidate);
+  } else {
+    console.log('Ingrese comandos válidos, --stats y --validate');
+  }
+}

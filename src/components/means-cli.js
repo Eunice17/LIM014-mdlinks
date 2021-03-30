ca4d76c0181ccab45cbc4511176bcffd1af9f1fc
Eunice Fiorella <euniceancajima@gmail.com>
2021-03-30 04:21:02 -0500

    Funciones complementarias en lógica CLI

---
+++ src/components/means-cli.js
@@ -0,0 +1,16 @@
+const links = ((msg) => {
+  console.log(msg);
+});
+const statsMd = (() => {
+  return 'Estado de md';
+});
+
+const statsValidateMd = (() => {
+  return 'Stats validate .md';
+});
+
+module.exports = {
+  links,
+  statsMd,
+  statsValidateMd,
+};

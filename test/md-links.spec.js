5dd8396002d70b35e6b0e67fb83ed7da727c0964
Eunice Fiorella <euniceancajima@gmail.com>
2021-04-01 20:11:42 -0500

    Actualizando los test

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -3,16 +3,11 @@ const {
   isFile,
   isDir,
   validateLinks,
-  returnObject,
 } = require('../src/components/means');
 const {
-  pathLocation,
-  searchMd,
-  arrayDir,
   mdLinks,
 } = require('../src/mdLinks');
 
-const array = [];
 describe('mdLinks', () => {
   it('Is function', () => {
     expect(typeof mdLinks).toBe('function');
@@ -36,43 +31,6 @@ describe('mdLinks', () => {
   test('Case type .txt', () => {
     return expect(mdLinks(data.obj[0].pathTxt, false))
       .rejects.toStrictEqual(new Error('La ruta apunta a un archivo con diferente extensión Marckdown, type: .txt'));
-  });
-});
-describe('Function pathLocation', () => {
-  it('Is function', () => {
-    expect(typeof pathLocation).toBe('function');
-  });
-  test('Should print Promises links validate', () => {
-    const promises = pathLocation(data.obj[0].path2, true);
-    Promise.all(promises).then((msg) => {
-      expect(msg).toEqual(data.obj[0].rst);
-    });
-  });
-  test('Should print Promises object links', () => {
-    const promesas = pathLocation(data.obj[0].path2, false);
-    Promise.all(promesas).then((msg) => {
-      expect(msg).toEqual(data.obj[0].rst2);
-    });
-  });
-});
-describe('Function searchMd', () => {
-  it('Is function', () => {
-    expect(typeof searchMd).toBe('function');
-  });
-  test('Should print arrayDir', () => {
-    expect(searchMd(data.obj[0].dir, array)).toEqual(data.obj[0].arrayDir);
-  });
-});
-
-describe('Function arayDir', () => {
-  it('Is function', () => {
-    expect(typeof arrayDir).toBe('function');
-  });
-  test('Should print information directory', () => {
-    const esto = arrayDir(data.obj[0].arrayDir, true);
-    const arrayPromises = esto.map((element) => Promise.all(element));
-    return expect(Promise.all(arrayPromises))
-      .resolves.toEqual(data.obj[0].salida);
   });
 });
 describe('Function isDirectory', () => {
@@ -100,15 +58,6 @@ describe('Function validateLinks', () => {
   });
   test('Case validateLinks cero', () => {
     return expect(validateLinks([]))
-      .toEqual([]);
-  });
-});
-describe('Function returnObject', () => {
-  it('Should print is a function', () => {
-    expect(typeof returnObject).toBe('function');
-  });
-  test('Case returnObject cero', () => {
-    return expect(returnObject([]))
       .toEqual([]);
   });
 });

61f49ecae277a479eb0ae2813b2aa2a3020baa58
Eunice Fiorella <euniceancajima@gmail.com>
2021-04-12 19:23:50 -0500

    Links sin estado

--- src/components/status.js
+++ src/components/status.js
@@ -3,17 +3,25 @@ const https = require('https');
 
 const awaitStatusValidate = ((link, ht) => {
   return new Promise((resolve) => {
-    ht.get(link.href, (res) => {
-      if (res.statusCode >= 404 && res.statusCode <= 599) {
-        link.status = res.statusCode;
-        link.statusText = 'Fail';
-        resolve(link);
-      } else {
-        link.status = res.statusCode;
-        link.statusText = 'Ok';
-        resolve(link);
-      }
-    });
+    ht
+      .get(link.href, (res) => {
+        if (res.statusCode >= 404 && res.statusCode <= 599) {
+          link.status = res.statusCode;
+          link.statusText = 'Fail';
+          resolve(link);
+        } else {
+          link.status = res.statusCode;
+          link.statusText = 'Ok';
+          resolve(link);
+        }
+      })
+      .on('error', (err) => {
+        if (err.code === 'ECONNREFUSED') {
+          link.status = 'No status';
+          link.statusText = 'Fail';
+          resolve(link);
+        }
+      });
   });
 });
 
